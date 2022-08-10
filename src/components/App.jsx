326b4f6b3e6b0976ba19da2ccf7b120137d187b0
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-08-10 18:16:16 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Section } from './Section/Section';
 import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { ContactsList } from './ContactsList/ContactsList';
-
+import { ThreeDots } from 'react-loader-spinner';
 import { useSelector } from 'react-redux';
 
 export const App = () => {
@@ -15,9 +15,24 @@ export const App = () => {
       </Section>
 
       <Section title="Contacts">
+        {loading && (
+          <ThreeDots
+            height="80"
+            width="80"
+            radius="9"
+            color="royalblue"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+            wrapperClassName=""
+            visible={true}
+          />
+        )}
         <Filter />
+
         <ContactsList />
-        {loading && <h4>In progress...</h4>}
       </Section>
     </>
   );
